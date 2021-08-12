8fc1e72f55cead63f93e6f485b147415c48c86a3
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-08-12 15:10:14 +0700

    X01AD: havoc ify

--- havoc_X01AD.mk
+++ havoc_X01AD.mk
@@ -14,13 +14,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit device stuff
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Device identifiers.
-PRODUCT_NAME := lineage_X01AD
+PRODUCT_NAME := havoc_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max M2
